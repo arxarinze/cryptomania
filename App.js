448296c46e4435f67a97d102f99e17b6559894f3
tnk <arinze@curacel.ai>
2021-09-21 23:06:17 +0100

    initial setup of libraries and skeletal structure

---
+++ App.js
@@ -0,0 +1,92 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+import Papa from 'papaparse';
+import DocumentPicker from 'react-native-document-picker';
+import {View, Card, CardProps, Button, Text} from 'react-native-ui-lib';
+import {
+  Colors,
+  Header,
+} from 'react-native/Libraries/NewAppScreen';
+
+const CryptoWidget = ({ children }): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [currency, setCurrency] = useState([]);
+  const uploadCSV = async () => {
+    
+  }
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Button title='Upload Csv' onPress={uploadCSV} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            flex: 1
+          }}>
+          {currency.map((file) => (
+            <CryptoWidget key={file}>
+            {file}
+          </CryptoWidget>
+          ))}
+          
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    marginBottom:32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
